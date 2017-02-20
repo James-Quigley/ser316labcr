fb737a87468d4846039cfde2a2f2da78ec767465
James Quigley <james@quigley.us>
2017-02-20 15:14:41 -0700

    Fixes #7

--- src/banking/primitive/core/AccountServerFactory.java
+++ src/banking/primitive/core/AccountServerFactory.java
@@ -1,23 +1,48 @@
+/*
+ * File: AccountServerFactory.java
+ * Author: Kevin Gary
+ * Date: February 20, 2017
+ *
+ * Description: Singleton class for an Account Server
+ */
 package banking.primitive.core;
 
-
+/*
+ * Class: AccountServerFactory
+ *
+ * Description: Singleton class that can create a new ServerSolution
+ */
 public class AccountServerFactory {
-
-	protected static AccountServerFactory singleton = null;
 
 	protected AccountServerFactory() {
 
 	}
 
+	/**
+	 * Method: getMe()
+	 * Inputs:
+	 * Returns: AccountServerFactory
+	 *
+	 * Description: Singleton Constructor
+	 */
 	public static AccountServerFactory getMe() {
-		if (singleton == null) {
-			singleton = new AccountServerFactory();
+		if (_singleton == null) {
+			_singleton = new AccountServerFactory();
 		}
 
-		return singleton;
+		return _singleton;
 	}
 
+	/**
+	 * Method: lookup()
+	 * Inputs:
+	 * Returns: AccountServer
+	 *
+	 * Description: Gets an account server, and the singleton ensures only one is used
+	 */
 	public AccountServer lookup() {
 		return new ServerSolution();
 	}
+
+	protected static AccountServerFactory _singleton = null;
 }
